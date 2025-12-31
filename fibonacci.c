3ab9d9776e0792b0be8c9e24e18f2d7eab32e2bb
Feiyun Chen <adenchen0219@gmail.com>
2025-12-31 11:41:17 -0500

    first version

---
+++ fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fibonacci(int nth)
+{
+    int a = 0;
+    int b = 1;
+   
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{   
+    printf("Which Fibonacci number do you want to know?\n");
+    printf("Please input ->: ");    
+
+    int n;
+    scanf("%d", &n);
+    
+    printf("Your input is: %d\n", n);
+
+    return 0;
+}
