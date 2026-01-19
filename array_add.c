492fba2bca476f7b6ac1e8acff10ac3c953758cd
Feiyun Chen <adenchen0219@gmail.com>
2026-01-19 15:32:37 -0500

    new file

---
+++ array_add.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int add(int a[], int len1, int b[], int len2, int r[], int len3)
+{
+    // list to store regrouping digits
+    int *p = (int *) malloc(len3 * 4);
+
+    for (int i=0; i<len3; ++i)
+    {
+        p[i] = 0;
+    }
+
+    /* Add the digits in the shorter array:
+     * a[] = {1,2,3,4,5,6}
+     * b[] = {9,7,5}
+     * len = 3
+     * r[] = {0,0,9}
+     */
+    int len = (len1 < len2) ? len1 : len2;
+
+    for (int i=0; i<len; ++i)
+    {
+        int t = a[i] + b[i] + p[i];
+        if (t >= 10)
+        {
+            p[i+1] = 1;
+        }
+        
+        r[i] = t % 10; // unit digit of t
+    }
+    
+    // the longer array 
+    int *x = (len1 > len2) ? a : b;
+
+    /* Work on the rest of the longer array: 
+     * add each digit with the regrouping digit, if any.
+     */
+
+    for (int i = len; i < (len3 - 1); ++i)
+    {
+        int t = x[i] + p[i];
+        if (t >= 10) 
+        {
+            p[i+1] = 1;
+        }
+
+        r[i] = t % 10;
+    } 
+   
+    // highest digit
+    r[len3-1] = p[len3-1];
+
+    free(p); p = NULL;
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Please input 2 numbers!\n");
+        return 1;
+    }
+
+    // copy argv[1] to a[]
+    int length1 = strlen(argv[1]);
+    int *a = (int *) malloc(length1 * 4);
+    
+    for (int i=0; i<length1; ++i)
+    {
+        a[i] = argv[1][length1 - (1 + i)] - '0';
+    }
+    
+    // copy argv[2] to b[]
+    int length2 = strlen(argv[2]);
+    int *b = (int *) malloc(length2 * 4);
+
+    for (int i=0; i<length2; ++i)
+    {
+        b[i] = argv[2][length2-1-i] - '0';
+    }
+
+    // larger of length1 and length2, and plus 1.
+    int length3 = (length1 > length2) ? length1+1 : length2+1;
+
+    int *r = (int *) malloc(length3 * 4);
+    add(a, length1, b, length2, r, length3);
+
+    for (int i=length3-1; i>=0; --i)
+    {
+        printf("%d", r[i]);
+    }
+    printf("\n");
+
+    free(a); a = NULL;
+    free(b); b = NULL;
+    free(r); r = NULL;
+
+    return 0;
+}
