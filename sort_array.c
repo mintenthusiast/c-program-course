dd4796f67b4ed4d118d7515f607339b167a3ff0f
Feiyun Chen <adenchen0219@gmail.com>
2026-02-07 12:03:22 -0500

    added function to locate minimum

--- sort_array.c
+++ sort_array.c
@@ -2,6 +2,25 @@
 #include<stdlib.h>
 #include<string.h>
 
+int min_loc(int *a, int len)
+{
+   int j = 0;
+   int min = a[0];
+
+   for (int i = 0; i < len; ++i)
+   {
+      if(min > a[i])
+      {
+         min = a[i];
+         j = i;
+      }
+   }
+
+   printf("The min value is: %d, the index of the min is: %d\n", min, j);
+   
+   return 0;
+}
+
 int main(int argc, char *argv[])
 {
    if (argc != 3) 
@@ -31,8 +50,11 @@ int main(int argc, char *argv[])
       if(argv[1][i] == ',')
       {
          arr[j] = atoi(&argv[1][i+1]);
+         j += 1;
       }
-   }
+   } 
+
+   min_loc(arr, length);
 
    free(arr);
    arr = NULL;
