9b9d83cad00a9cbc82f733e3565afbb3c6ec1270
Feiyun Chen <adenchen0219@gmail.com>
2026-02-22 11:54:29 -0500

    added sorting function

--- read_file.c
+++ read_file.c
@@ -2,6 +2,50 @@
 #include<stdlib.h>
 #include<string.h>
 
+int print_array(int *a, int len);
+
+int min_loc_swap(int *a, int len)
+{
+   int j = 0;
+   int min = a[0];
+
+   for (int i = 0; i < len; ++i)
+   {
+      if(min > a[i])
+      {
+         min = a[i];
+         j = i;
+      }
+   }
+
+   a[j] = a[0];
+   a[0] = min;
+  
+   return 0;
+}
+
+int print_array(int *a, int len)
+{
+   for (int i = 0; i < len; ++i)
+   {
+      printf("%d ", a[i]);
+   }
+   printf("\n");
+
+   return 0;
+}
+
+int sort_array(int *a, int len)
+{ 
+   for (int i = 0; i < len - 1; ++i)
+   {
+      min_loc_swap(a + i, len - i);
+   }
+
+   return 0;
+}
+
+
 int print_file (char *filename)
 {
    FILE *fp = fopen(filename, "r");
@@ -15,24 +59,30 @@ int print_file (char *filename)
    char buf[255];
 
 
-   // not hard-cded 255
+   // not hard-coded 255
    
    char *p = fgets(buf, sizeof(buf), fp);
-   char *token = strtok(buf, ",");
+   char *token = strtok(buf, ",\n");
 
    while (p != NULL)
    {
+      int a[10];    
+      int counter = 0;
+    
       while (token != NULL)
       {
-         int val = atoi(token);
-         printf("%d ", val);
+         a[counter] = atoi(token);
+         counter += 1;
  
-         token = strtok(NULL, ",");
+         token = strtok(NULL, ",\n");
       }
-
+      
       p = fgets(buf, sizeof(buf), fp);
       token = strtok(buf, ",");
-      printf("\n");
+
+      // sort and print 
+      sort_array(a, counter);
+      print_array(a, counter);
    }
 
    printf("finished reading the file\n");
