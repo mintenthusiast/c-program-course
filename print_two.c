c5b6d3c7cefdd1e8d035c75ef999807e745d552a
Feiyun Chen <adenchen0219@gmail.com>
2026-04-19 12:02:37 -0400

    fix

--- print_two.c
+++ print_two.c
@@ -8,10 +8,11 @@ int main(int argc, char* argv[])
    int n = argc - 1;
    int* arr = (int *) malloc (sizeof(int) * n);
 
-   for (int i = 0; i < n; ++i) { if (atoi(argv[i+1]) % 5 == 0) { return 1; } arr[i] = atoi(argv[i+1]); }
+   for (int i = 0; i < n; ++i) { arr[i] = atoi(argv[i+1]); }
 
    for (int i = 0; i < n; ++i) 
    {
+      if (arr[i] % 5 == 0) { break; }
       if ((i + 1) % 2 == 0)
       {     
       printf("%d ", arr[i]); 
