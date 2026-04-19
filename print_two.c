56b1c544ad04d686767bfe25c3b3295b9900cd6e
Feiyun Chen <adenchen0219@gmail.com>
2026-04-19 11:50:26 -0400

    breaks on 5

---
+++ print_two.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "myarray.h"
+
+int main(int argc, char* argv[])
+{
+
+   int n = argc - 1;
+   int* arr = (int *) malloc (sizeof(int) * n);
+
+   for (int i = 0; i < n; ++i) { arr[i] = atoi(argv[i+1]); }
+
+   for (int i = 0; i < n; ++i) 
+   {
+      if ((i + 1) % 2 == 0)
+      {     
+         if ((arr[i] % 5) == 0)
+         {
+            break;
+         }
+
+      printf("%d ", arr[i]); 
+      }
+   }
+
+   printf("\n");
+
+   free(arr);
+   arr = NULL;
+
+   return 0;
+}
