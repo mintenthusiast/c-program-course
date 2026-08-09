87c90fdd2e6fb5156ed0c4eb5c773375400ef872
mintenthusiast <adenchen0219@gmail.com>
2026-08-09 11:49:30 -0400

    spin

--- game1.c
+++ game1.c
@@ -3,10 +3,36 @@
 #include <unistd.h>
 #include <conio.h>
 
-void print_stars(int x, int y, int row_max, int col_max)
+void print_stars(int x, int y, int row_max, int col_max, int rotate)
 {
-	int arrx[4] = {x, x, x, x + 1};
-	int arry[4] = {y, y + 1, y + 2, y + 2};
+	int arrx[4];
+	int arry[4];
+
+	switch (rotate % 4)
+	{
+		case 0:
+			arrx[0] = x; arrx[1] = x; arrx[2] = x; arrx[3] = x + 1;
+			arry[0] = y; arry[1] = y + 1; arry[2] = y + 2; arry[3] = y + 2; 
+			break;
+
+		case 1:
+			arrx[0] = x; arrx[1] = x + 1; arrx[2] = x + 2; arrx[3] = x;
+			arry[0] = y; arry[1] = y; arry[2] = y; arry[3] = y + 1;
+			break;
+
+		case 2:
+			arrx[0] = x; arrx[1] = x + 1; arrx[2] = x + 1; arrx[3] = x + 1;
+			arry[0] = y; arry[1] = y; arry[2] = y + 1; arry[3] = y + 2;
+			break;
+
+		case 3:
+			arrx[0] = x + 2; arrx[1] = x; arrx[2] = x + 1; arrx[3] = x + 2;
+			arry[0] = y; arry[1] = y + 1; arry[2] = y + 1; arry[3] = y + 1;
+			break;
+
+		default:
+			exit(0);
+	}
 
 	int counter = 0;
 
@@ -36,7 +62,12 @@ int main(int argc, char* argv[])
 	int x = atoi(argv[3]);
 	int y = atoi(argv[4]);
 
-	print_stars(x, y, row_max, col_max);
+	print_stars(x, y, row_max, col_max, 0);
+	int rotate = 0;
+
+	int w = 2;
+	int h = 3;
+
 	char input;
 
 	while (1)
@@ -57,14 +88,19 @@ int main(int argc, char* argv[])
 	
 			else if (input == 'd')
 			{
-				if (x < col_max - 2) x++;	
+				if (x < col_max - w) x++;	
+			}
+
+			else if (input = 'r')
+			{
+				if (x < col_max - w && y < row_max - h) rotate++; int tmp = h; h = w; w = tmp;
 			}	
 		}
 
 		
-		if (y < col_max - 3) y++; // move down
+		if (y < col_max - h) y++; // move down
 
-		print_stars(x, y, row_max, col_max);
+		print_stars(x, y, row_max, col_max, rotate);
 		sleep(1);
 
 	}
